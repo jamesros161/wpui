80780e7ed8d9543e92d2c8f16fb355104f23dae5
James Rosado <dev@twmsllc.com>
2019-06-27 06:08:17 -0400

    test

--- BodyWidgets.py
+++ BodyWidgets.py
@@ -41,9 +41,24 @@ class installs(BodyWidget):
         self.app.L.debug(' kwargs : %s', kwargs)
         home_text = self.app.W.get_text('body', 'Obtaining List of available WordPress Installations','center')
         return U.Filler(home_text, 'middle')
-    def after_action(self,*args,**kwargs):
-        self.app.L.debug('args: %s, kwargs: %s', args,kwargs)
-
+    def after_action(self,installations):
+        installation_columns = [W.get_col_row([
+            W.get_text('header', 'Location', 'center'),
+            W.get_text('header','Home URL', 'center'),
+            W.get_text('header','Valid wp_options Table','center'),
+            W.get_text('header','Passed wp_db_check','center')
+        ])]
+        for installation in installations:
+            installation_columns.append(
+                W.get_col_row([
+                    W.get_text('body', installation['directory'], 'center'),
+                    W.get_text('body', installation['home_url'], 'center'),
+                    W.get_text('body', installation['valid_wp_options'],'center'),
+                    W.get_text('body', installation['wp_db_check_success'],'center')
+                ])
+            )
+        installation_pile = U.Pile(installation_columns)
+        self.app.frame.set_contents('body',[installation_pile, None])
 class plugins(BodyWidget):
     def __init__(self,app,user_args=None,calling_view=None):
         BodyWidget.__init__(self,app)
