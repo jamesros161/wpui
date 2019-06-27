6ca5d8c0ef75a0667714bca6afde6e0c88f33742
James Rosado <dev@twmsllc.com>
2019-06-27 05:42:55 -0400

    test

--- actions.py
+++ actions.py
@@ -5,5 +5,5 @@ class Actions():
     def get_installations(self):
         self.app.L.debug("get_installations Action Started")
         installations = Installations(self.app)
-        active_view = self.app.state.get_active_view
+        active_view = self.app.state.active_view
         self.app.loop.event_loop.remove_enter_idle(active_view.action_handler)
