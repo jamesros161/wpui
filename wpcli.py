0ca91850955daa1947a4055af789609005dcec96
James Rosado <dev@twmsllc.com>
2019-06-27 03:40:35 -0400

    test

--- wpcli.py
+++ wpcli.py
@@ -1,11 +1,31 @@
-import os, sys, getpass, subprocess
+import os, sys, getpass, subprocess, json
 class Installations():
     def __init__(self,L):
+        call = Call(L)
         self.username = getpass.getuser()
         self.homedir = os.path.expanduser('~%s' % self.username)
         installation_dirs = []
         for root, dirs, files in os.walk(self.homedir, topdown=True):
             if 'wp-config.php' in files:
+                call.wpcli(root,['db','check'])
                 installation_dirs.append(root)
                 #print(os.path.join(root, name))
         L.debug("WP Installation Directories for user %s: %s", self.username, installation_dirs)
+class Call():
+    def __init__(self,L):
+        self.L = L
+    def wpcli(self, path, arguments):
+
+        popen_args = ['wp']
+        for argument in arguments:
+            popen_args.append(argument)
+        popen_args.extend(['--path='+path, '--format=json'])
+
+        reqOutput, reqError = subprocess.Popen(popen_args, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE
+            ).communicate()
+        data = json.loads(reqOutput)
+        error = json.loads(reqError)
+        self.L.debug("Data: %s \nError: %s", data,error)
+
