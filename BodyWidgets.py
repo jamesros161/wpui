543caf30544de5c852f1fdc5b58dee7fa94d9351
James Rosado <dev@twmsllc.com>
2019-06-27 06:14:02 -0400

    test

--- BodyWidgets.py
+++ BodyWidgets.py
@@ -58,7 +58,8 @@ class installs(BodyWidget):
                 ])
             )
         installation_pile = U.Pile(installation_columns)
-        self.app.frame.contents.__setitem__('body',[installation_pile, None])
+        filler = U.Filler(installation_pile, 'middle')
+        self.app.frame.contents.__setitem__('body',[filler, None])
         self.app.loop.draw_screen()
 class plugins(BodyWidget):
     def __init__(self,app,user_args=None,calling_view=None):
