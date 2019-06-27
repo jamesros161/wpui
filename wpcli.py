4022ebcccdefabd304003deb5f2290879b49484c
James Rosado <dev@twmsllc.com>
2019-06-27 04:17:30 -0400

    test

--- wpcli.py
+++ wpcli.py
@@ -1,4 +1,5 @@
 import os, sys, getpass, subprocess, json
+from datetime import datetime
 class Installations():
     def __init__(self,L):
         L.debug('Installations Initialized')
@@ -6,7 +7,7 @@ class Installations():
         self.username = getpass.getuser()
         self.homedir = os.path.expanduser('~%s' % self.username)
         installations = []
-        for root, dirs, files in os.walk(self.homedir, topdown=True):
+        for root,_,files in os.walk(self.homedir, topdown=True):
             if 'wp-config.php' in files:
                 x = {
                     'directory' : root,
@@ -15,14 +16,14 @@ class Installations():
                     'wp_db_check_success' : False,
                     'wp_db_error' : ''
                 }
-                data,error = call.wpcli(root,['db','check'])
+                data,error = call.wpcli(root,['db','check','--skip-{plugin,theme}s'])
                 x['error'] = error
                 if data:
                     data = data.splitlines()
                     for line in data:
                         if '_options' in line and 'OK' in line:
                             x['valid_wp_options'] = True
-                            homedata,_ = call.wpcli(root,['option','get','home'])
+                            homedata,_ = call.wpcli(root,['option','get','home','--skip-{plugin,theme}s'])
                             if homedata:
                                 x['home_url'] = homedata
                         if 'Success: Database checked' in line:
@@ -34,6 +35,8 @@ class Call():
     def __init__(self,L):
         self.L = L
     def wpcli(self, path, arguments):
+        run_starttime = datetime.now()
+        self.L.debug("WPCLI Call Started: %s", run_starttime)
         popen_args = ['wp']
         for argument in arguments:
             popen_args.append(argument)
@@ -48,5 +51,7 @@ class Call():
         #data = json.loads(reqOutput)
         #error = json.loads(reqError)
         #self.L.debug("Data: %s \nError: %s", data,error)
+        command_runtime = datetime.now() - run_starttime
+        self.L.debug("WPCLI Command Runtime: %s", command_runtime)
         return data,error
 
