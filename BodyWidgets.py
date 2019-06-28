7ebf754d36b30189bd47dafc4990bdc49167b6f0
James Rosado <dev@twmsllc.com>
2019-06-28 07:33:10 -0400

    test

--- BodyWidgets.py
+++ BodyWidgets.py
@@ -97,7 +97,7 @@ class database(BodyWidget):
     def __init__(self,app,user_args=None,calling_view=None):
         super(database,self).__init__(app)
         if not self.app.state.active_installation:
-            self.app.view.installs.activate()
+            self.app.views.installs.activate()
     def define_widget(self, **kwargs): 
         self.app.L.debug(' kwargs : %s', kwargs)
         main_text = self.app.W.get_text('body', 'Polling for Database Information','center')
