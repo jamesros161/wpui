0f7097d904c24bad66229799f38a29e22946d1df
James Rosado <dev@twmsllc.com>
2019-06-28 00:39:18 -0400

    test

--- actions.py
+++ actions.py
@@ -7,5 +7,5 @@ class Actions():
         if not hasattr(self,'installations'):
             self.installations = Installations(self.app)
         active_view = self.app.state.active_view
-        self.app.loop.event_loop.remove_enter_idle(active_view.action_handler)
+        #self.app.loop.event_loop.remove_enter_idle(active_view.action_handler)
         self.app.views.installs.body.after_action(self.installations.installations)
